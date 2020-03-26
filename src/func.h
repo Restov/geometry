4f3fbd9f0dac9ed71246f854c8d153b1f69eb095
Restov <romankotovv@gmail.com>
2020-03-27 01:58:07 +0700

    Update Header

--- src/func.h
+++ src/func.h
@@ -1,6 +1,6 @@
 #ifndef __FUNC_H
 #define __FUNC_H
 void GCircle(float x, float y, float r);
-void GTriangle(float x1,float x2, float y1, float y2);
+void GTriangle(float x1,float x2, float x3, float y1, float y2, float y3);
 int prompt_menu_item();
 #endif
