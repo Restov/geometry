28789318cdbda952caa6797b230d4647183f957a
Restov <romankotovv@gmail.com>
2020-03-27 20:43:06 +0700

    Add x3, y3

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,7 @@ int main()
         break;
     }
     case 2: {
-        cin >> x1 >> x2 >> y1 >> y2;
+        cin >> x1 >> x2 >> x3 >> y1 >> y2 >> y3;
         GTriangle(x1, x2, x3, y1, y2, y3);
         break;
     }
