a3e2c1a02407a411c8ac994d78455b1ef8c7dcea
Restov <romankotovv@gmail.com>
2020-04-26 20:28:19 +0700

    Add zero test

--- test/test.cpp
+++ test/test.cpp
@@ -9,6 +9,10 @@ TEST(Circle, isCheck) {
 TEST(Circle, isNotCheck) {
   bool a = CircleCheck("qweqwewq", "123", "dada");
   bool b = CircleCheck(" ", " ", " ");
+  bool minus = CircleCheck("-1", "2", "3");
+  bool zero = CircleCheck("1", "2", "0");
   ASSERT_EQ(false, a);
   ASSERT_EQ(false, b);
+  ASSERT_EQ(false, minus);
+  ASSERT_EQ(false, zero);
 }
