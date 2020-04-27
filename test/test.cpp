abe9c44d909b9ad0d2c0b47fe21b526c7cb8700f
Restov <romankotovv@gmail.com>
2020-04-27 22:08:59 +0700

    Change string to char

--- test/test.cpp
+++ test/test.cpp
@@ -1,18 +1,20 @@
 #include "circle.h"
 #include <gtest/gtest.h>
-TEST(Circle, isCheck) {
-  bool k = CircleCheck("1", "2", "3");
-  bool c = CircleCheck("1235.123", "64234.13123", "1256.1421");
-  ASSERT_EQ(true, k);
-  ASSERT_EQ(true, c);
+TEST(Circle, isCheck)
+{
+    bool k = CircleCheck('1', '2', '3');
+    bool c = CircleCheck('1235.123', '64234.13123', '1256.1421');
+    ASSERT_EQ(true, k);
+    ASSERT_EQ(true, c);
 }
-TEST(Circle, isNotCheck) {
-  bool a = CircleCheck("qweqwewq", "123", "dada");
-  bool b = CircleCheck(" ", " ", " ");
-  bool minus = CircleCheck("-1", "2", "3");
-  bool zero = CircleCheck("1", "2", "0");
-  ASSERT_EQ(false, a);
-  ASSERT_EQ(false, b);
-  ASSERT_EQ(false, minus);
-  ASSERT_EQ(false, zero);
+TEST(Circle, isNotCheck)
+{
+    bool a = CircleCheck('qweqwewq', '123', 'dada');
+    bool b = CircleCheck(' ', ' ', ' ');
+    bool minus = CircleCheck('-1', '2', '3');
+    bool zero = CircleCheck('1', '2', '0');
+    ASSERT_EQ(false, a);
+    ASSERT_EQ(false, b);
+    ASSERT_EQ(false, minus);
+    ASSERT_EQ(false, zero);
 }
